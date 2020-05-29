ef8018a8ba0bc5bf16f131fb3e00706540f0458d
PrishaPattojoshi0 <63450542+PrishaPattojoshi0@users.noreply.github.com>
2020-05-29 13:16:07 +0530

    C-23

--- sketch.js
+++ sketch.js
@@ -1,15 +1,19 @@
 var rect1,rect2;
-var box1,box2,box3;
+var box1,box2,box3,box4,box5;
 
 function setup() {
   createCanvas(800,500);
 
-  rect1 = createSprite(230,300,10,400);
-  rect2 = createSprite(570,300,10,400);
+  rect1 = createSprite(130,300,10,400);
+  rect2 = createSprite(750,300,10,400);
 
-  box1 = createSprite(300,335,90,330);
-  box2 = createSprite(400,340,90,320);
-  box3 = createSprite(500,335,90,330);
+  box1 = createSprite(200,335,80,330);
+  box2 = createSprite(320,360,130,280);
+  box3 = createSprite(440,335,80,330);
+  box4 = createSprite(560,360,130,280);
+  box3 = createSprite(680,335,80,330);
+
+  triangle();
 }
 
 function draw() {
@@ -17,3 +21,8 @@ function draw() {
   
   drawSprites();
 }
+
+triangle(x1,y1,x2,y2,x3,y3);
+x1,y1 = first point;
+x2,y2 = second point;
+x3,y3 = third point;
